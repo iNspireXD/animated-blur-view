e19952d4d63d94d27a742d219a0d142ae8932f50
Aswin Karki <aswin.karki44@gmail.com>
2023-08-19 12:37:09 +0545

    added image card and its background

--- App.tsx
+++ App.tsx
@@ -1,11 +1,54 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Dimensions,
+  FlatList,
+  Image,
+} from "react-native";
+import { data } from "./data";
+
+const { width, height } = Dimensions.get("screen");
+
+const imageW = width * 0.7;
+const imageH = imageW * 1.54;
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
+      <View style={StyleSheet.absoluteFillObject}>
+        {data.map((image, index) => {
+          return (
+            <Image
+              key={`image-${index}`}
+              source={{ uri: image }}
+              style={[StyleSheet.absoluteFillObject]}
+              blurRadius={10}
+            />
+          );
+        })}
+      </View>
+      <FlatList
+        horizontal
+        scrollEventThrottle={16}
+        pagingEnabled
+        data={data}
+        keyExtractor={(_, index) => index.toString()}
+        renderItem={({ item }) => {
+          return (
+            <View
+              style={{ width, alignItems: "center", justifyContent: "center" }}
+            >
+              <Image
+                source={{ uri: item }}
+                style={{ width: imageW, height: imageH }}
+              />
+            </View>
+          );
+        }}
+      />
     </View>
   );
 }
@@ -13,8 +56,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
